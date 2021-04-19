5fc2c8431d8b6dbb825def7236d1aecbba5e46c0
nhporti2 <nhporti2@gmail.com>
2021-04-19 17:24:50 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,38 +1,42 @@
 
 parkingSpaces = []
+currentTicket = {}
 
-class Garage():
+#
+
+class parkingGarage():
   ticket = ["1", "2", "3", "4", "5", "6", "7". "8", "9", "10"]
   def __init__(self, parking_space, tickets, parking_status)
     self.parking_space = parking_space
     self.tickets = tickets
     self.parking_status
 
+#general greeting
+    
 def Garage(self):
   print("Welcome to Parking Garage! Please take available ticket.")
-    for ticket in self.tickets:
-      print(ticket)
-
-def payForParking(self)
-  ticket = input("Welcome to the payment kiosk, what is your ticket number? ")
-        if ticket in parking_status:
-            self.parking_status[ticket]= "This ticket has been paid. You have 15 min to exit parking garage. Have a nice day!"
-
-def takeTicket(ticket):
+      print(garage)
+ 
+#take ticket
+      
+ def takeTicket(ticket):
   clear_output()
       for i in range(10):
         ticket.append(i)
   print(ticket)
-
-currentTicket = {
-  parkingSpace1 = 1,
-  parkingSpace2 = 2,
-  parkingSpace3 = 3,
-  parkingSpace4 = 4,
-  parkingSpace5 = 5,
-  parkingSpace6 = 6,
-  parkingSpace7 = 7,
-  parkingSpace8 = 8,
-  parkingSpace9 = 9,
-  parkingSpace10 = 10
-}
+        
+def parkingStatus(self)
+  clear_output()
+    if self.parkingStatus == []:
+      print("There are no available parking spaces at this time.")
+    else 
+    
+#pay for parking and exit
+    
+def payForParking(self)
+  clear_output()
+      if ticket in parking_status:
+        self.parking_status[ticket]= "You have 15 min to exit parking garage. Have a nice day!" + + str(self.parkingStatus)
+          break
+        
+print(parkingGarage)
